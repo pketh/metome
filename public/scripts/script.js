aa9d976183fa1a7d8d6fc7832eb9fcd142a2719a
Pirijan <pirijan@gmail.com>
2013-11-18 16:01:13 -0500

    Going to abandon content-editable for textarea + dedicated img field

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,4 @@
-
-
-
-
-
-
-
-
-
-
+// Metome.io Client
 
 
 
@@ -16,15 +7,14 @@
 $(document).ready(function () {
 
   var socket = io.connect('http://localhost:8000');
-
   var user = 'pirijan'; // TODO - auth
 
   socket.on('connect', function () { 
-    socket.emit('login', user );  
-    socket.emit('entries'); // requesting entry titles
+    socket.emit('login', user );
+    // 
   });
 
-  // list entries
+  // list entries (triggered by connection)
   socket.on('entriesSuccessful', function(titles){
     console.log('entries successfully retrieved');
     titles.forEach(function (titleIndex) {
@@ -32,13 +22,10 @@ $(document).ready(function () {
         $(".entriesList").append('<li data-id = "' + titleIndex._id + '">Blank entry.</li>');
       } else {
         $(".entriesList").append('<li data-id = "' + titleIndex._id + '">' + titleIndex.title+ '</li>');
-        // for non blank, simply append what the server gives --> it'll be prefiltered/formatted >>>>>>>>>>>>>>>>>>>>>>>>>>>
       };
     });
   });
 
-
-  
   // request an entry
   $('.entriesList').on('click', 'li', function(){
     var entryID = $(this).data('id');
@@ -46,11 +33,11 @@ $(document).ready(function () {
     socket.emit('entry', entryID);
   });
 
-  // load the entry based on template (entry.html)
-  socket.on('entrySuccessful', function(entry, entryMonthStr, entryYear, entryID){    
-    $('.entryContainer').load('entry.html', function() {
+  // load the entry based on template (entry.html) + runs for newEntry
+  socket.on('entrySuccessful', function(entry, entryMonth, entryYear, entryID){    
+    $('.entryContainer').load('entryTemplate.html', function() {
       
-      printEntry(entry, entryMonthStr, entryYear, entryID);
+      printEntry(entry, entryMonth, entryYear, entryID);
       
       // title saving
       $('.title[contenteditable]').typing({
@@ -92,15 +79,13 @@ $(document).ready(function () {
         viewEntries(entryID);
       });
 
-
     });
-    
   }); // close entry
 
   $('.settings').on('click', function(){
     // 
+    console.log('settings clicked')
   });  
-
 
   // make new entry
   $('.newEntry').on('click', function(){
@@ -113,16 +98,17 @@ $(document).ready(function () {
     socket.emit('entry', entryID);
   });
 
+  // showing successful save
   socket.on('saveSuccess', function(entryID) {
     console.log('Saved ' + entryID); // DEBUG
-    displaySaved();  
+    displaySaved();
   });
 
+  // when entry sucessfully removed
   socket.on('removeEntrySuccess', function(entryID) {
     entryListRemove(entryID);
     viewEntries(entryID);
   });
-
 
 
 
@@ -133,18 +119,20 @@ $(document).ready(function () {
     // "disconnect" is emitted when the socket disconnected    
   });
 
-  socket.on('reconnect_failed', function(){
-    // reconnect failed - emitted when socket.io fails to re-establish a working 
-  });
-
   socket.on('reconnecting', function(){
     // reconnecting - is emitted when the socket is attempting to reconnect with the server
+  });
+  
+  socket.on('reconnect_failed', function(){
+    // reconnect failed - emitted when socket.io fails to re-establish a working 
   });
 
   socket.on('reconnect', function(){
     // Q: reconnect fires when connection emits (automatically?)
     // reconnect - emitted when socket.io successfully reconnected to the server
     // on a reconnect event switch contenteditables back to true
+    // make sure the list doesn't append again to replicate on top of existing
+    console.log('reconnect event hit')
   });
 
 
@@ -153,14 +141,6 @@ $(document).ready(function () {
 
 // --------------------------------------------------------------------------
 
-function printEntry(entry, entryMonthStr, entryYear, entryID) {
-  console.log('fetched entry template')    
-  $('.entry').attr('data-id', entryID);
-  $('.entry .title').append(entry.title);
-  $('.entry .content').append(entry.content);
-  $('.status').append('<span>' + entryMonthStr + ', ' + entryYear + '</span>');
-  console.log( 'printed entry for ' + entryID); // DEBUG
-}
 
 
 function entryListRemove(entryID) {
@@ -174,18 +154,16 @@ function saveTitle(event, entryID) {
   socket.emit('titleEdited', { title: newTitle }, entryID);
   console.log('Saving... ' + 'title ' + entryID); // DEBUG  
   console.log(newTitle); // DEBUG
-  
 // DO NOW !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // November 16, 2013
 // find the entry in the titlelist and update it to its latest title
-  
 }
+
 
 function saveContent(event, entryID) {
   var newContent = $('.content').html();
   socket.emit('contentEdited', { content: newContent }, entryID);
-  console.log(newContent); // DEBUG
-  console.log('Saving... ' + 'content: on stop typing ' + entryID); // DEBUG
+  console.log('Saving... ' + newContent + ' entryID ' + entryID); // DEBUG
 }
 
 
@@ -195,83 +173,77 @@ function displaySaving() {
 
 function displaySaved() {
   $('.status span').replaceWith('<span>' + 'Saved' + '</span>');
-
-// TBD??
-// transition back to showing the date .. if yes, based on what rules?
 }
 
 // November 16, 2013
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // back to viewing the entries list
 function viewEntries(entryID) { // triggered by .done & browser back.
-
-  console.log('verifying that viewEntries has ID: ' + entryID);
-
+  console.log('verifying that viewEntries has ID: ' + entryID);  
   // move the view back to the entries list (do later)
-  
   $('.entry').remove();
-    
   // if entryID exists && li with entryID exists in .entrieslist : 
-// entries .foreach li .removeClass ('updated') 
-
-
+  // entries .foreach li .removeClass ('updated') 
   // remove all existing .updateds on the li
   // add an .updated css class on the li with the correct data entryid
 }
 
 
+// figure out scheme / interaction rules for adding images
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// TXT FILTERS > MOVE ALL THIS TO SERVER for what's passed to the client for entrieslist >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// title filters DONT change the actual db record
-
-// function formatTitle(titleIndex) {
-//   console.log('formatting title ' + titleIndex.title); // checks out
-//   var entryID = titleIndex._id;
-//   var title = titleIndex.title;
-
-//     dottify(title);
-//     capitalize(title);
-//     $(".entriesList").append('<li data-id = "' + entryID + '">' + title + '</li>');
-//   }
-// }
-// 
-// 
-//filter 1:
-// function dottify(title) {
-//   var lastLetter = title.substring(title.length - 1);
-//   if (lastLetter !== '.' and also '?' and also '!' and also '~' ) <- may need to use html codes for these? hopefulyl not.
-//) {
-//     var dottedTitle = title.concat(title, '.'); // the full title with a .
-//   }
-// }
-// 
-//filter 2:
-// function capitalize(title) {
-//   var firstLetter = title.substring(0,1);
-//   var capitalizedTitle = title.replace(firstLetter, firstLetter.toUpperCase());
-// }
-
-// CONTENT TXT FILTERS formatting and dotifying not working. this actually changes the db record ==============================> server
-
-// function formatContent() {
-  // firstchar check..
-  // change to content soap filter on server >>>>>>>>>>>>
-// }
+function printEntry(entry, entryMonth, entryYear, entryID) {
+  switch (entryMonth)
+  {
+  case 0:
+    var entryMonthStr = 'Jan';
+    break;
+  case 1:
+    var entryMonthStr = 'Feb';
+    break;
+  case 2:
+    var entryMonthStr = 'Mar';
+    break;
+  case 3:
+    var entryMonthStr = 'Apr';
+    break;
+  case 4:
+    var entryMonthStr = 'May';
+    break;
+  case 5:
+    var entryMonthStr = 'Jun';
+    break;
+  case 6:
+    var entryMonthStr = 'Jul';
+    break;
+  case 7:
+    var entryMonthStr = 'Aug';
+    break;
+  case 8:
+    var entryMonthStr = 'Sep';
+    break;
+  case 9:
+    var entryMonthStr = 'Oct';
+    break;
+  case 10:
+    var entryMonthStr = 'Nov';
+    break;
+  case 11:
+    var entryMonthStr = 'Dec';
+    break;
+  default:
+    var entryMonthStr = 'err';
+    break;
+  }
+  console.log('fetched entry template');
+  $('.entry').attr('data-id', entryID);
+  $('.entry .title').append(entry.title);
+  $('.entry .content').append(entry.content);
+  $('.status').append('<span>' + entryMonthStr + ', ' + entryYear + '</span>');
+  console.log( 'printed entry for ' + entryID); // DEBUG
+}
 
 
 
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// Metome.io Server
+
+
+
+
+
 var MongoClient = require('mongodb'),
   ObjectId = require('mongodb').ObjectID,
   connect = require('connect'),
@@ -6,10 +12,6 @@ var MongoClient = require('mongodb'),
   fs = require('fs');
   // --> npm install lean mean router here ..
   // --> auth
-  
-// --> refactor: local require files = closures.js , api (for json curl reqs)
-
-Metome = {}; // Globally scoped object
 
 io.configure('development', function(){
   io.set('log level', 2); // default is 3 (shows full debug)
@@ -26,83 +28,34 @@ io.sockets.on('connection', function(socket) {
     var user = username;
     
     MongoClient.connect('mongodb://localhost/metome', function(err, db) {
-      if (err) throw err;  
+      if (err) throw err;
       var collection = db.collection(user);
     
-      collection.find( {}, { title : 1 } ).sort( { _id: -1 } ).toArray(function(err, titles) { // query db -> get titles in reverse order by id/cdate -> convert to array -> make titles object [{x,y}, {x,y}]
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      // entries successful emits on connect/load bc not gated
+      collection.find( {}, { title : 1 } ).sort( { _id: -1 } ).toArray(function(err, titles) {
         if (err) throw err;
-        socket.emit('entriesSuccessful', titles)
+        socket.emit('entriesSuccessful', titles);
       });
 
-      // listen for titles request  
-      socket.on('entries', function(){   
-      });
       
       // load individual entry
       socket.on('entry', function(entryID){
         console.log(entryID); // DEBUG        
-        collection.findOne({ _id : ObjectId ( entryID ) },function(err,entry) { // pulls in entry object
-          if (err) throw err;
+        collection.findOne({ _id : ObjectId ( entryID ) },function(err,entry) {
+          if (err) throw err;          
           var entryDate = new Date( (ObjectId(entryID).getTimestamp()) )
           var entryMonth = entryDate.getMonth()
-          switch (entryMonth)
-          {
-          case 0:
-            var entryMonthStr = 'Jan';
-            break;
-          case 1:
-            var entryMonthStr = 'Feb';
-            break;
-          case 2:
-            var entryMonthStr = 'Mar';
-            break;
-          case 3:
-            var entryMonthStr = 'Apr';
-            break;
-          case 4:
-            var entryMonthStr = 'May';
-            break;
-          case 5:
-            var entryMonthStr = 'Jun';
-            break;
-          case 6:
-            var entryMonthStr = 'Jul';
-            break;
-          case 7:
-            var entryMonthStr = 'Aug';
-            break;
-          case 8:
-            var entryMonthStr = 'Sep';
-            break;
-          case 9:
-            var entryMonthStr = 'Oct';
-            break;
-          case 10:
-            var entryMonthStr = 'Nov';
-            break;
-          case 11:
-            var entryMonthStr = 'Dec';
-            break;
-          default:
-            var entryMonthStr = 'err';
-            break;
-          }
-          var entryYear = entryDate.getFullYear()
-          
-          socket.emit('entrySuccessful', entry, entryMonthStr, entryYear, entryID); 
+          var entryYear = entryDate.getFullYear()  
+          socket.emit('entrySuccessful', entry, entryMonth, entryYear, entryID);
         });
       });
-      
-      // db.pirijan.findOne({ '_id' : ObjectId("5279262e74d92da751eb2b8e") })
 
-// console.log(Metome.entryID) // global id scope test
-// returns Thu Sep 20 2012 22:56:39 GMT-0400
-      
       // write title on changes
-      socket.on('titleEdited', function(newTitle, entryID) {      
+      socket.on('titleEdited', function(newTitle, entryID) {
         collection.update(
-          { '_id' : ObjectId ( entryID ) }, // id should be passed in from client
-          { $set: 
+          { '_id' : ObjectId ( entryID ) },
+          { $set:
            newTitle
           },
           function (err) {
@@ -110,13 +63,14 @@ io.sockets.on('connection', function(socket) {
             console.log(newTitle); // DEBUG
             console.log(entryID); // DEBUG
             socket.emit('saveSuccess', entryID);
-        });
+          }
+        );
       });    
       
       // write content on changes
-      socket.on('contentEdited', function(newContent, entryID) {
+      socket.on('contentEdited', function(newContent, entryID) {        
         collection.update(
-          { '_id' : ObjectId ( entryID ) }, // id should be passed in from client
+          { '_id' : ObjectId ( entryID ) },
           { $set: 
            newContent
           },
@@ -125,26 +79,20 @@ io.sockets.on('connection', function(socket) {
             console.log(newContent); // DEBUG
             console.log(entryID) // DEBUG
             socket.emit('saveSuccess', entryID);
-        });
+          }
+        );
       });
-          
       
+      // insert new record
       socket.on('newEntry', function(err){
         if (err) throw err;
-        // make new mongo record in user collection
-        // November 14, 2013
-        // change doc defaults to span class ph 'text'
         var document =  {title:'', content:''}
-        
         collection.insert(document, function(err, entry) {
           if (err) throw err;
-          
-          console.log("Record added as "+entry[0]._id);
           var entryID = entry[0]._id
-          
+          console.log("Record added as "+ entryID);
           socket.emit('newEntrySuccess', entryID);
         });
-
       });
   
       socket.on('removeEntry', function(entryID){
@@ -176,22 +124,8 @@ io.sockets.on('connection', function(socket) {
 }); // closes socket.io
 
 
-// TODO 
-// November 14, 2013
-// refactor out functions here (client style)
 
 
-// BUG p wrapping new line issue - too aggressive with the p's 
-// MOVE TO SERVER as part of textfilter 
-  // $('.content[contenteditable]').on('keyup', function(event) {
-  //   if(event.which == 13) // enter key
-  //     console.log('wrap fired'); // DEBUG
-  //     $(".content").contents().filter(function() {
-  //       return this.nodeType == 3;
-  //     }).wrap('<p></p>');
-  // });
-// Or something more regex-y
-//
 
 
 
